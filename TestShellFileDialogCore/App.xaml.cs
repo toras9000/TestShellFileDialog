053dfebfd2f17670f03efef084ab1ebe3e0437c3
toras9000 <toras9000@example.com>
2020-08-29 02:13:53 +0900

    アプリケーションをPrismに乗せる

--- TestShellFileDialogCore/App.xaml.cs
+++ TestShellFileDialogCore/App.xaml.cs
@@ -5,13 +5,31 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Prism.Ioc;
+using TestShellFileDialogCore.Views;
 
 namespace TestShellFileDialogCore
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        /// <summary>
+        /// 型の登録処理
+        /// </summary>
+        /// <param name="containerRegistry">型登録</param>
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        /// <summary>
+        /// アプリケーションシェルの作成
+        /// </summary>
+        /// <returns></returns>
+        protected override Window CreateShell()
+        {
+            return this.Container.Resolve<MainWindow>();
+        }
     }
 }
---
+++ TestShellFileDialogCore/ViewModels/AppViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Mvvm;
+
+namespace TestShellFileDialogCore.ViewModels
+{
+    /// <summary>
+    /// アプリケーションのベースViewModel
+    /// </summary>
+    public class AppViewModelBase : BindableBase
+    {
+    }
+}
